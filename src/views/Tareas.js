186ac690787d5fc0fee2a0dbe8ca66f0007f9af3
Jesus Hernandez <jesushdzusa@gmail.com>
2024-05-10 11:16:33 -0500

    reubicacion de actividad 1.2

--- src/views/Tareas.js
+++ src/views/Tareas.js
@@ -8,14 +8,15 @@ export const Tareas = () => {
             
            <details>
             <summary className="subjects">Fundamentos de Programacion II</summary>
+            
+           </details>
+           <details>
+            <summary className="subjects">Estructuras Algebraicas</summary>
             <details>
                 <summary className="asigments">Actividad 1.2 Linea del tiempo</summary>
                 <iframe src='https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=14cyYjVmr472bI7FeupyxQhf4INtDiYRaoe_OskcKhXU&font=Default&lang=en&initial_zoom=2&height=650' width='100%' height='650' webkitallowfullscreen mozallowfullscreen allowfullscreen frameborder='0'></iframe>
             </details>
             
-           </details>
-           <details>
-            <summary className="subjects">Estructuras Algebraicas</summary>
            </details>
            <details>
             <summary className="subjects">Interfaces de usuario basadas en web</summary>
